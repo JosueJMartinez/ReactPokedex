c9018e484abfa9b0387f058f83fcd7c9e90328da
JosueJMartinez <jmarti61@gmail.com>
2020-08-31 12:04:11 -0500

    Updated for better quality of images and missing alt in images

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -5,12 +5,20 @@ export default class PokeCard extends Component {
 	render() {
 		const { id, type, name, exp } = this.props;
 		// console.log(props);
-
+		function paddingID(oldID) {
+			const stringID = oldID + '';
+			if (stringID.length === 1) return '00' + stringID;
+			else if (stringID.length === 2) return '0' + stringID;
+			return oldID;
+		}
 		return (
 			<div className="PokeCard">
 				<h2 className="PokeCard-title">{name}</h2>{' '}
 				<img
-					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+					src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddingID(
+						id
+					)}.png`}
+					alt={`${name} picture`}
 				/>
 				<p>Type: {type}</p>
 				<p className="PokeCard-p">EXP: {exp}</p>{' '}
