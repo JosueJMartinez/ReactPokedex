3f2ac8532e69c8efad432176610b4be2fb4fda88
JosueJMartinez <jmarti61@gmail.com>
2020-08-31 23:08:08 -0500

    Added missing file from last push

---
+++ src/components/PokeGame.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Pokedex from './Pokedex';
+
+export default class PokeGame extends Component {
+	render() {
+		const pokemon = [
+			{ id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
+			{ id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
+			{ id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
+			{
+				id: 12,
+				name: 'Butterfree',
+				type: 'flying',
+				base_experience: 178
+			},
+			{
+				id: 25,
+				name: 'Pikachu',
+				type: 'electric',
+				base_experience: 112
+			},
+			{
+				id: 39,
+				name: 'Jigglypuff',
+				type: 'normal',
+				base_experience: 95
+			},
+			{ id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
+			{ id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
+		];
+
+		function rand4Poke() {
+			const hand = [];
+			for (let i = 0; i < 4; i++) {
+				const rand = Math.floor(Math.random() * pokemon.length);
+				hand.push(pokemon[rand]);
+			}
+			return hand;
+		}
+		return (
+			<div>
+				<Pokedex pokemon={rand4Poke()} />
+				<Pokedex pokemon={rand4Poke()} />
+			</div>
+		);
+	}
+}
