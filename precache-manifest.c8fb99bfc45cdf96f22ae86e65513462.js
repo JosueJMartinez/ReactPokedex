160f800fcc46118db38b40e35891a928424691ad
JosueJMartinez <jmarti61@gmail.com>
2020-11-16 02:08:59 -0600

    Updates

---
+++ precache-manifest.c8fb99bfc45cdf96f22ae86e65513462.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb30f2a2654ef1b7a33624bde3e6a23",
+    "url": "/ReactPokedex/index.html"
+  },
+  {
+    "revision": "d1248336f257ea012010",
+    "url": "/ReactPokedex/static/css/2.dd12d4dd.chunk.css"
+  },
+  {
+    "revision": "2629fff98097aa4eb5c5",
+    "url": "/ReactPokedex/static/css/main.e5133025.chunk.css"
+  },
+  {
+    "revision": "d1248336f257ea012010",
+    "url": "/ReactPokedex/static/js/2.5dc5c887.chunk.js"
+  },
+  {
+    "revision": "dc72335b0c81df5e2f574a8a9b692250",
+    "url": "/ReactPokedex/static/js/2.5dc5c887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2629fff98097aa4eb5c5",
+    "url": "/ReactPokedex/static/js/main.1ed9b4a3.chunk.js"
+  },
+  {
+    "revision": "818aa585bd359e80da56",
+    "url": "/ReactPokedex/static/js/runtime-main.6d8515d1.js"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/ReactPokedex/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/ReactPokedex/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/ReactPokedex/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/ReactPokedex/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/ReactPokedex/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/ReactPokedex/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/ReactPokedex/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/ReactPokedex/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/ReactPokedex/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/ReactPokedex/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/ReactPokedex/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/ReactPokedex/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/ReactPokedex/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/ReactPokedex/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/ReactPokedex/static/media/fa-solid-900.8e1ed89b.woff2"
+  }
+]);
